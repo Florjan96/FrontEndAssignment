1b91a2600f95ee936457ef249af733e1898ef6d5
Florjan96 <florianshiqerukaj@yahoo.com>
2023-05-09 09:01:51 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div className="App">
+
+   
+
+      <div className="images">
+    
+    <div>
+      <img src="images/b9.jpg" width="367px"/>
+      </div>
+    <div><img src="images/tefo2.jpg" width="367px"/></div>
+    <div><img src="images/tefo3.jpg" width="367px"/></div>
+    <div><img src="images/tefo4.jpg" width="367px"/></div>
+    </div>
+     </div>
+  )
+}
+
+export default App
